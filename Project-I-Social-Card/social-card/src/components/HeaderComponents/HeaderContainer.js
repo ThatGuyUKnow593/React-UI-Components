26bc9f9d69f5c32081fde8efa89aaea1c83d7b1e
ThatGuyUKnow593 <trenteuclide409@gmail.com>
2019-02-27 21:17:46 -0600

    all content in page just need to work on styling

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import './Header.css';
+
+import HeaderTitle from "./HeaderTitle";
+import ImageThumbnail from "./ImageThumbnail";
+import HeaderContent from "./HeaderContent";
+
+function HeaderContainer() {
+  return(
+    <div className='header'>
+      <ImageThumbnail />
+
+      <div className='headerContent'>
+        <HeaderTitle firstName="Lambda" lastName="School" />
+        <HeaderContent />
+      </div>
+    </div>
+  );
+}
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle(props){
+  let mainName = props.firstName + ' ' + props.lastName;
+  let atName = props.firstName + '' + props.lastName;
+
+  let n = '26 jan';
+
+  return (
+    <div className='name'>
+      <p>{mainName}</p>
+      <h4>{atName}</h4>
+      <h5>{n}</h5>
+    </div>
+  );
+}
+
+export default HeaderTitle;
